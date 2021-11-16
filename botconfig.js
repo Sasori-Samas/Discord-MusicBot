d04ef7900ed807b96b4e32b7fd588eeb7db4f044
Sasori-Samas <77126624+Sasori-Samas@users.noreply.github.com>
2021-11-16 17:27:54 -0500

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -21,7 +21,7 @@ module.exports = {
   
   Presence: {
     status: "online", // You can show online, idle, and dnd
-    name: "Prefix (+)"
+    name: "Prefix (+) Enjoy the Music", // The message shown
     type: "LISTENING", // PLAYING, WATCHING, LISTENING, STREAMING
   },
 
